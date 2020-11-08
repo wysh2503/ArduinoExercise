ba159ef8006ea608cef610f27735ec4656f416ee
Winston Yeung <74127616+wysh2503@users.noreply.github.com>
2020-11-08 15:07:06 +0800

    Add files via upload

---
+++ IR/IR.ino
@@ -0,0 +1,19 @@
+#include <IRremote.h>
+int IRpin=9;
+IRrecv IR(IRpin);
+decode_results cmd;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+IR.enableIRIn();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+while (IR.decode(&cmd)==0){
+  }
+Serial.println(cmd.value,HEX);
+delay(1500);
+IR.resume();  
+}
